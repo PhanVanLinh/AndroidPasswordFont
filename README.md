eff3ab686c624103f9df36c7385d11a73bce889c
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-09-05 11:10:02 +0700

    [Improvement] Update README file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AndroidPasswordFont
 
-DEMO for answer here https://stackoverflow.com/a/52176331/5381331
+### DEMO for answer here
+- https://stackoverflow.com/a/52176331/5381331
+- https://stackoverflow.com/a/52178340/5381331
+
+### Relevant question
+`EditText` will crash when long click in some device if using `<item name="android:fontFamily">sans-serif-light</item>`
+https://stackoverflow.com/a/52421199/5381331
--- app/src/main/java/vn/linh/androidpasswordfont/MainActivity.java
+++ app/src/main/java/vn/linh/androidpasswordfont/MainActivity.java
@@ -4,10 +4,8 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.EditText;
-
-import static android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +17,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edtPassword = findViewById(R.id.edit_text_password);
 
-        Typeface cache = edtPassword.getTypeface();
-        edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        edtPassword.setTypeface(cache);
+        enableInputHiddenPassword(edtPassword);
 
+        findViewById(R.id.button_show_hide_password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible(edtPassword)) {
+                    enableInputHiddenPassword(edtPassword);
+                } else {
+                    enableInputVisiblePassword(edtPassword);
+                }
+                edtPassword.setSelection(edtPassword.getText().length());
+            }
+        });
+    }
+
+    final int INPUT_TYPE_VISIBLE_PASSWORD =
+            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
+    final int INPUT_TYPE_HIDDEN_PASSWORD =
+            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+
+    private boolean isPasswordVisible(EditText editText) {
+        return editText.getInputType() == INPUT_TYPE_VISIBLE_PASSWORD;
+    }
+
+    private void enableInputVisiblePassword(EditText editText) {
+        Typeface cache = editText.getTypeface();
+        editText.setInputType(INPUT_TYPE_VISIBLE_PASSWORD);
+        editText.setTypeface(cache);
+    }
+
+    private void enableInputHiddenPassword(EditText editText) {
+        Typeface cache = editText.getTypeface();
+        editText.setInputType(INPUT_TYPE_HIDDEN_PASSWORD);
+        editText.setTypeface(cache);
     }
 }
